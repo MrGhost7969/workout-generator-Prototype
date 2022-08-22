0599fa7a8356eac03f28fd76bcaec095ad217497
Franco <bigghostyman1234@gmail.com>
2022-08-22 14:40:36 -0400

    Add info

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import '../index.css';
 
-function Header(){
+function Header() {
     return (
-        <div className='bg-sky-600 p-6 flex flex-wrap justify-center ...' />
+        <>
+            <div className='bg-sky-600 p-6 flex flex-wrap justify-center ...' >
+                <h1 className='text-white'>Resets in the morning, afternoon, and evening</h1>
+            </div>
+        </>
     );
 }
 
