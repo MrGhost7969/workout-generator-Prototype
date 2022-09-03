2fa19ed26605a98e1deda3fb934550a35a62ad53
Franco <bigghostyman1234@gmail.com>
2022-09-03 19:38:24 -0400

    Remove unnecessary <>

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,11 +3,9 @@ import '../index.css';
 
 function Header() {
     return (
-        <>
-            <div className='bg-sky-600 p-6 flex flex-wrap justify-center ...' >
-                <h1 className='text-white'>Resets in the morning, afternoon, and evening</h1>
-            </div>
-        </>
+        <div className='bg-sky-600 p-6 flex flex-wrap justify-center ...' >
+            <h1 className='text-white'>Resets in the morning, afternoon, and evening!</h1>
+        </div>
     );
 }
 
